8b64b8b18f751f8720282f93e11fe9e302ca355a
Anders Romin <anders.romin@apegroup.com>
2015-08-07 15:24:00 +0200

    geolocation

--- angularpizza.js
+++ angularpizza.js
@@ -75,6 +75,7 @@ angular.module('angularPizza', ['ngRoute', 'ngGeolocation'])
                 var distance = -1;
                 angular.forEach(restaurants.model, function (rest) {
                     var d2 = restaurants.distanceToRestaurant(myPosition, rest);
+                    rest.distance = Math.round(d2 * 1000);
                     if (distance < 0 || distance > d2) {
                         closest = rest;
                         distance = d2;
@@ -87,11 +88,33 @@ angular.module('angularPizza', ['ngRoute', 'ngGeolocation'])
         };
 
         restaurants.distanceToRestaurant = function(myPosition, restaurant) {
+            return distance(myPosition.coords.longitude, myPosition.coords.latitude, restaurant.longitude, restaurant.latitude);
+
             var a = myPosition.coords.latitude - restaurant.latitude;
             var b = myPosition.coords.longitude - restaurant.longitude;
             var c = Math.sqrt(a * a + b * b);
             return c;
         };
+
+        /// code taken from http://stackoverflow.com/questions/13840516/how-to-find-my-distance-to-a-known-location-in-javascript
+        function distance(lon1, lat1, lon2, lat2) {
+            var R = 6371; // Radius of the earth in km
+            var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
+            var dLon = (lon2-lon1).toRad();
+            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+                Math.sin(dLon/2) * Math.sin(dLon/2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            var d = R * c; // Distance in km
+            return d;
+        }
+
+        /** Converts numeric degrees to radians */
+        if (typeof(Number.prototype.toRad) === "undefined") {
+            Number.prototype.toRad = function() {
+                return this * Math.PI / 180;
+            }
+        }
 
         restaurants.getClosestRestaurant();
     });
